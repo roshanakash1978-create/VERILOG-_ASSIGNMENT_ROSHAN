6c1b4f8689e47c4563b4d8196f11d2125813fc93
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-09 20:50:13 +0530

    Update and rename tb_top.v to tb_bcd_adder.v

---
+++ day1/BCD_Adder/Tb/tb_bcd_adder.v
@@ -0,0 +1,14 @@
+module tb_bcd_adder;
+    reg [3:0] A;
+    reg [3:0] B;
+    reg cin;
+    wire [3:0] Sum;
+    wire cout;
+    bcd_adder dut(.A(A),.B(B),.cin(cin),.Sum(Sum),.cout(cout));
+    initial begin
+        A = 4'd3; B = 4'd4; cin = 0; #10;
+        A = 4'd7; B = 4'd6; cin = 0; #10;
+        A = 4'd4; B = 4'd5; cin = 0; #10;
+        A = 4'd9; B = 4'd9; cin = 1; #10;
+    end
+endmodule
