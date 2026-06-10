69381933261c83167d457f4e5613bfab197a40a4
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-10 13:58:39 +0530

    Update sequencedetect.v

--- day3/sequence_detector /design/sequencedetect.v
+++ day3/sequence_detector /design/sequencedetect.v
@@ -1,1 +1,59 @@
-
+module sequencedetect(input clk , reset,din, output reg detected);
+  parameter idle=2'b00;
+  parameter s1=2'b01;
+  parameter s2=2'b10;
+  parameter s3=2'b11;
+  reg [1:0] ps, ns;
+  
+  always @(posedge clk)
+    begin
+      if(reset)
+        begin
+          ps<=idle;
+        end
+      else 
+        ps<=ns;
+    end
+      
+  always@(*)
+    begin
+      case(ps)
+        idle: begin
+          detected=0;
+          if(din==0)
+            begin
+              ns=idle;
+            end
+          else
+            ns=s1;
+        end
+        
+        s1:begin
+          if(din==0)
+            begin
+              ns=s2;
+            end
+          else
+            ns=s1;
+        end
+        
+        s2:begin
+          if(din==0)
+            ns=idle;
+          else
+            ns=s3;
+        end
+        
+        s3:begin
+          if(din==0)
+            begin
+              ns=idle;
+              detected=1;
+            end
+          else
+            ns=s1;
+        end
+      endcase
+    end
+  
+endmodule
