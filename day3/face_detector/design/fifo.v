a1d7e7043c91d50ce283dda9f3fc902eb8d0cae1
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-10 20:36:03 +0530

    Update fifo.v

--- day3/face_detector/design/fifo.v
+++ day3/face_detector/design/fifo.v
@@ -1,27 +1,33 @@
-module fifo(input clk,rst,wrenb,rdenb,
-input [7:0]data_in,
-output reg [7:0] data_out,
-output reg full,empty
-    );
-    reg [7:0] mem [7:0];
-    reg [2:0] wr_ptr=0;
-    reg [2:0] rd_ptr=0;
-    integer i;
-    always @(posedge clk)
-    begin
-    if(rst) begin
-    for(i=0;i<8;i=i+1)
-    mem[i]<=0;
-    end
-    else if(wrenb==1 && full==0) begin
-    mem[wr_ptr]<=data_in;
-    wr_ptr<=wr_ptr+3'b001;
-    end
-    else if(rdenb==1 && empty==0) begin
-    data_out<=mem[rd_ptr];
-    rd_ptr<=rd_ptr+3'b001;
-    end
-    assign full=((wr_ptr+3'b001)==rd_ptr)?1'b1:1'b0;
-    assign empty=(wr_ptr==rd_ptr)?1'b1:1'b0;
+module fifo(input clk, rst, wr_en, rd_en,
+    input [7:0] data_in,
+    output reg [7:0] data_out,
+    output full, empty
+);
+    
+    reg [7:0] mem [7:0]; 
+    reg [2:0] wr_ptr = 0;
+    reg [2:0] rd_ptr = 0;
+
+    assign full = ((wr_ptr + 3'b001) == rd_ptr) ? 1'b1 : 1'b0;
+    assign empty = (wr_ptr == rd_ptr) ? 1'b1 : 1'b0;
+
+    always @(posedge clk) begin
+        if(rst) begin
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            data_out <= 0;
+          
+        end
+        else begin
+            if(wr_en == 1 && full == 0) begin
+                mem[wr_ptr] <= data_in;
+                wr_ptr <= wr_ptr + 3'b001;
+            end
+            
+            if(rd_en == 1 && empty == 0) begin
+                data_out <= mem[rd_ptr];
+                rd_ptr <= rd_ptr + 3'b001;
+            end
+        end
     end 
 endmodule
