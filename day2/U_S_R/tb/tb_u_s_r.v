e32b15d4530d5d24c64bf66e84fedc14275e612b
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-10 15:14:03 +0530

    Update tb_u_s_r.v

--- day2/U_S_R/tb/tb_u_s_r.v
+++ day2/U_S_R/tb/tb_u_s_r.v
@@ -1,30 +1,81 @@
-module usr_tb;
-  wire [3:0] DATAOUT;
-  reg clock, reset;
-  reg [1:0] MODE;
-  reg [3:0] DATAIN;  
-  usr dut(DATAOUT, clock, reset, MODE, DATAIN);  
-  always #5 clock = ~clock;  
-  initial begin 
-    $dumpfile("dump.vcd"); 
-    $dumpvars(0, usr_tb);
-    $monitor("Time=%0dt | Reset=%b | Mode=%b | DataIn=%b | DataOut=%b", $time, reset, MODE, DATAIN, DATAOUT);
-  end
-  initial begin
-    clock = 0; 
-    MODE = 2'b00; 
-    DATAIN = 4'b0000;
-    reset = 1; #10; 
-    reset = 0; #10;
-    MODE = 2'b00; reset = 1; #10; reset = 0; #10;
-    MODE = 2'b01; DATAIN = 4'b0011; #10;
-    MODE = 2'b01; DATAIN = 4'b0011; #10;
-    MODE = 2'b00; reset = 1; #10; reset = 0; #10;
-    MODE = 2'b10; DATAIN = 4'b0111; #10;
-    MODE = 2'b10; DATAIN = 4'b0111; #10;
-    MODE = 2'b00; reset = 1; #10; reset = 0; #10;
-    MODE = 2'b11; DATAIN = 4'b1010; #10;
-    #20;
-    $finish;
-  end  
+module tb_universal_shift_register;
+
+    reg clk;
+    reg rst;
+    reg [1:0] mode;
+    reg serial_in;
+    reg [3:0] parallel_in;
+
+    wire serial_out;
+    wire [3:0] parallel_out;
+
+    universal_shift_register uut (
+        .clk(clk),
+        .rst(rst),
+        .mode(mode),
+        .serial_in(serial_in),
+        .parallel_in(parallel_in),
+        .serial_out(serial_out),
+        .parallel_out(parallel_out)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(1, tb_universal_shift_register);
+
+        clk = 0;
+        rst = 1;
+        mode = 2'b00;
+        serial_in = 0;
+        parallel_in = 4'b0000;
+
+        $monitor("Time=%0dns | Reset=%b | Mode=%b | SerialIn=%b | ParallelIn=%b | SerialOut=%b | ParallelOut=%b",
+                 $time, rst, mode, serial_in, parallel_in, serial_out, parallel_out);
+
+        #15;
+        rst = 0;
+        #5;
+
+        $display("\n--- Testing PIPO Mode (11) ---");
+        mode = 2'b11;
+        parallel_in = 4'b1011;
+        #10;
+        
+        parallel_in = 4'b0101;
+        #10;
+
+        $display("\n--- Testing PISO Mode (10) ---");
+        mode = 2'b10;
+        #10;
+        #10;
+        #10;
+        #10;
+
+        $display("\n--- Testing SISO Mode (00) ---");
+        mode = 2'b00;
+        
+        serial_in = 1; #10;
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+        serial_in = 1; #10;
+        
+        serial_in = 0; #10;
+        serial_in = 0; #10;
+        serial_in = 0; #10;
+        serial_in = 0; #10;
+
+        $display("\n--- Testing SIPO Mode (01) ---");
+        mode = 2'b01;
+        
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+
+        $display("\n--- Simulation Finished ---");
+        $finish;
+    end
+     
 endmodule
