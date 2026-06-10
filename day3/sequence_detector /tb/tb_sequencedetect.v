346690f16949de48031d2071ea9f07525533d5a4
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-10 13:59:09 +0530

    Update tb_sequencedetect.v

--- day3/sequence_detector /tb/tb_sequencedetect.v
+++ day3/sequence_detector /tb/tb_sequencedetect.v
@@ -1,1 +1,37 @@
+module tb_sequencedetect;
+  reg clk;
+  reg reset;
+  reg din;
+  wire detected;
 
+  sequencedetect uut (
+    .clk(clk), 
+    .reset(reset), 
+    .din(din), 
+    .detected(detected)
+  );
+
+  always begin
+    #10 clk = ~clk;
+  end
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1, tb_sequencedetect); 
+    clk = 0;
+    reset = 1;
+    din = 0;
+    #25;   
+    reset = 0;
+    #20;   
+    din = 1;
+    #20;    
+    din = 1;
+    #20;   
+    din = 1;
+    #20;   
+    din = 0;
+    #20;    
+    $finish;
+  end     
+endmodule
