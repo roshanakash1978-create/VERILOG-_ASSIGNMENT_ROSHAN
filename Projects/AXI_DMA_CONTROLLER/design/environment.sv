de6d0cd026bba50b4806f3302753a4ac47401e25
roshanakash1978-create <roshanakash1978@gmail.com>
2026-06-23 01:00:00 +0530

    Update environment.sv

--- Projects/AXI_DMA_CONTROLLER/design/environment.sv
+++ Projects/AXI_DMA_CONTROLLER/design/environment.sv
@@ -1,1 +1,21 @@
-environment
+class environment;
+    agent      agt; 
+    scoreboard scb;
+    
+    mailbox #(transaction) mon2scb;
+    virtual axi_dma_if vif;
+
+    function new(virtual axi_dma_if vif);
+        this.vif = vif;
+        mon2scb = new(); 
+        agt = new(vif, mon2scb); 
+        scb = new(mon2scb);
+    endfunction
+
+    task run(); 
+        fork 
+            agt.run(); 
+            scb.run(); 
+        join_any 
+    endtask
+endclass
